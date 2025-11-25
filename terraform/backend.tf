0b4d20c36482f35a8163ca5cc5a2f7f306a088b6
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-11-25 15:31:36 -0500

    Remove DynamoDB lock table - no more lock issues

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "sensor-backend-terraform-state"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    bucket  = "sensor-backend-terraform-state"
+    key     = "prod/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+    # Removed dynamodb_table - no more lock issues
   }
 }
